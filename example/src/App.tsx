1ca537a59f8bd9f349451eeab5a69570abaaa6cd
cocomine <cocomine@cocomine.cc>
2026-04-19 22:44:46 +0800

    fix: update RN 0.83.0

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,102 +1,101 @@
 import { useState } from 'react';
-import { Button, SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import { Button, StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { Profile, Shadowsocks } from 'react-native-shadowsocks-android';
 
 export default function App() {
     const isDarkMode = useColorScheme() === 'dark';
     const [profileID, setProfileID] = useState<number>(0);
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-            <Button
-                title={'Connect'}
-                onPress={() =>
-                    Shadowsocks.connect().then((a) => console.log(a))
-                }
-            />
-            <Button
-                title={'Disconnect'}
-                onPress={() => Shadowsocks.disconnect()}
-            />
-            <Button
-                title={'Switch Profile'}
-                onPress={() => console.log(Shadowsocks.switchProfile(3))}
-            />
-            <Button
-                title={'Add Profile'}
-                onPress={() => {
-                    const profile = new Profile(
-                        'us2.vpn.cocomine.cc',
-                        6381,
-                        'iLBKsPYgjCZgBZsTO6WsAVpb7/zwa5n0FWelWv+tNNY=',
-                        'aes-256-gcm'
-                    );
-                    profile.remoteDns = '1.1.1.1';
-                    profile.proxyApps = false;
-                    profile.individual = [
-                        'com.eg.android.AlipayGphone',
-                        'com.wudaokou.hippo',
-                    ];
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <StatusBar
+                    barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                />
+                <Button
+                    title={'Connect'}
+                    onPress={() =>
+                        Shadowsocks.connect().then((a) => console.log(a))
+                    }
+                />
+                <Button
+                    title={'Disconnect'}
+                    onPress={() => Shadowsocks.disconnect()}
+                />
+                <Button
+                    title={'Switch Profile'}
+                    onPress={() => console.log(Shadowsocks.switchProfile(3))}
+                />
+                <Button
+                    title={'Add Profile'}
+                    onPress={() => {
+                        const profile = new Profile(
+                            'us2.vpn.cocomine.cc',
+                            6381,
+                            'iLBKsPYgjCZgBZsTO6WsAVpb7/zwa5n0FWelWv+tNNY=',
+                            'aes-256-gcm'
+                        );
+                        profile.remoteDns = '1.1.1.1';
+                        profile.proxyApps = false;
+                        profile.individual = [
+                            'com.eg.android.AlipayGphone',
+                            'com.wudaokou.hippo',
+                        ];
 
-                    const id = Shadowsocks.addProfile(profile);
-                    console.log(profile);
-                    setProfileID(id);
-                }}
-            />
-            <Button
-                title={'import Profile Uri'}
-                onPress={() => {
-                    const profiles = Shadowsocks.importProfileUri(
-                        'ss://YWVzLTI1Ni1nY206aUxCS3NQWWdqQ1pnQlpzVE82V3NBVnBiNy96d2E1bjBGV2VsV3YrdE5OWT0=@us2.vpn.cocomine.cc:6381#shadowsocks-app'
-                    );
+                        const id = Shadowsocks.addProfile(profile);
+                        console.log(profile);
+                        setProfileID(id);
+                    }}
+                />
+                <Button
+                    title={'import Profile Uri'}
+                    onPress={() => {
+                        const profiles = Shadowsocks.importProfileUri(
+                            'ss://YWVzLTI1Ni1nY206aUxCS3NQWWdqQ1pnQlpzVE82V3NBVnBiNy96d2E1bjBGV2VsV3YrdE5OWT0=@us2.vpn.cocomine.cc:6381#shadowsocks-app'
+                        );
 
-                    console.log(profiles);
-                }}
-            />
-            <Button
-                title={'Delete Profile'}
-                onPress={() =>
-                    console.log(Shadowsocks.deleteProfile(profileID))
-                }
-            />
-            <Button
-                title={'Clear Profiles'}
-                onPress={() => Shadowsocks.clearProfiles()}
-            />
-            <Button
-                title={'List All Profiles'}
-                onPress={() => console.log(Shadowsocks.listAllProfile())}
-            />
-            <Button
-                title={'Get Profiles'}
-                onPress={() => console.log(Shadowsocks.getProfile(profileID))}
-            />
-            <Button
-                title={'Update Profile'}
-                onPress={() => {
-                    const profile = new Profile(
-                        'hk.vpn.cocomine.cc',
-                        6373,
-                        'i4tbxhk6uEk-LT$',
-                        'xchacha20-ietf-poly1305'
-                    );
-                    profile.id = profileID;
-                    profile.proxyApps = false;
-                    profile.name = 'hk vpn(edit)';
+                        console.log(profiles);
+                    }}
+                />
+                <Button
+                    title={'Delete Profile'}
+                    onPress={() =>
+                        console.log(Shadowsocks.deleteProfile(profileID))
+                    }
+                />
+                <Button
+                    title={'Clear Profiles'}
+                    onPress={() => Shadowsocks.clearProfiles()}
+                />
+                <Button
+                    title={'List All Profiles'}
+                    onPress={() => console.log(Shadowsocks.listAllProfile())}
+                />
+                <Button
+                    title={'Get Profiles'}
+                    onPress={() =>
+                        console.log(Shadowsocks.getProfile(profileID))
+                    }
+                />
+                <Button
+                    title={'Update Profile'}
+                    onPress={() => {
+                        const profile = new Profile(
+                            'hk.vpn.cocomine.cc',
+                            6373,
+                            'i4tbxhk6uEk-LT$',
+                            'xchacha20-ietf-poly1305'
+                        );
+                        profile.id = profileID;
+                        profile.proxyApps = false;
+                        profile.name = 'hk vpn(edit)';
 
-                    console.log(Shadowsocks.updateProfile(profile));
-                }}
-            />
-        </SafeAreaView>
+                        console.log(Shadowsocks.updateProfile(profile));
+                    }}
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
